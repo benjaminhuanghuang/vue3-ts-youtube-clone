d7afd2b6a2b7433636c2123c86024dc985851c31
benjamin huang <benjaminhuanghuang@gmail.com>
2022-03-24 11:13:04 -0700

    add files

---
+++ youtubeclone-frontend/src/layout/AppLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <AppHeader/>
+  <AppSidebar/>
+  <router-view/>
+</template>
+
+<script lang="ts">
+
+import AppHeader from './AppHeader.vue'
+import AppSidebar from './AppSidebar.vue'
+
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  components: {
+    AppHeader,
+    AppSidebar
+  }
+})
+</script>
